f6b29cd5e37a0c53e9749e58575cdc00a0ef3bfd
Manolo Schotsborg <manolo.schotsborg@outlook.com>
2016-02-02 12:44:31 -0200

    initial commit

---
+++ main.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var port = process.env.PORT || 3000;
+var multer = require('multer');
+var app = express();
+
+app.set('views', (__dirname + '/views'));
+app.set('view engine', 'jade');
+
+
+function index(req, res) {
+	res.setHeader('Content-type', 'text/html');
+	res.render('index');
+}
+
+function analyseFile(req, res) {
+	var fileSize = JSON.stringify({"file size": req.file.size})
+	res.setHeader('Content-type', 'text/plain');
+	res.send(fileSize);
+	
+}
+
+
+app.get('/', index);
+
+app.post('/api/fileanalyse', multer({dest: './uploads/'}).single('upload'), analyseFile);
+
+app.listen(port, function(){console.log('listening on localhost port: ' + port)});
